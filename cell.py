598c78ca1df88afaa1c2df4f9eb9eb996fdc9eef
Fahad Ahmed <fahad838@gmail.com>
2025-04-07 13:03:07 +0530

    Create a line that goes through middle of cells

--- cell.py
+++ cell.py
@@ -13,6 +13,9 @@ class Cell:
         self._win = window
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return 
+        
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
@@ -33,6 +36,25 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo = False):
+
+        side1 = abs(self._x2 - self._x1)
+        center1_x = self._x1 + side1 / 2
+        center1_y = self._y1 + side1 / 2
+
+        side2 = abs(to_cell._x2 - to_cell._x1)
+        center2_x = to_cell._x1 + side2 / 2
+        center2_y = to_cell._y1 + side2 / 2
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(center1_x,center1_y), Point(center2_x, center2_y))
+        self._win.draw_line(line, fill_color)
+        
+
 
         
 
--- main.py
+++ main.py
@@ -5,8 +5,22 @@ from cell import Cell
 
 def main():
     win = Window(800, 600)
-    cell = Cell(win)
-    cell.draw(50,50,100,100)
+    c1 = Cell(win)
+    c1.has_right_wall = False
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+
+    c3= Cell(win)
+    c3.has_top_wall = False
+
+
+    c1.draw(50,50,100,100)
+    c2.draw(100,50,150,100)
+    c3.draw(100,100,150,150)
+    c1.draw_move(c2)
+    c2.draw_move(c3)
     win.wait_for_close()
 
 main()
