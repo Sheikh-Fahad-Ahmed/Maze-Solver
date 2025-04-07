a02a471afd90cd7ea9ed4cb48dd8cbcb1bd993f4
Fahad Ahmed <fahad838@gmail.com>
2025-04-07 11:59:46 +0530

    Create a line using Point and Line class

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from window import Window
+from point import Point
+from line import Line
 
 def main():
     win = Window(800, 600)
+    p1 = Point(10,20)
+    p2 = Point(200,100)
+    line = Line(p1,p2)
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 main()
--- window.py
+++ window.py
@@ -5,13 +5,18 @@ class Window:
         self.__root = Tk()
         self.__root.title('Maze Solver')
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
 
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
 
     def wait_for_close(self):
         self.__running = True
