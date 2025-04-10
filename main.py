ad541c48fdd759531b265e5f02763c421c5588a3
Fahad Ahmed <fahad838@gmail.com>
2025-04-10 20:51:33 +0530

    Add method to break the entrance and exit walls and test it

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
 from window import Window
-from point import Point
-from line import Line
-from cell import Cell
+from maze import Maze
 
 def main():
-    win = Window(800, 600)
-    c1 = Cell(win)
-    c1.has_right_wall = False
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
 
-    c2 = Cell(win)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
-    c3= Cell(win)
-    c3.has_top_wall = False
-
-
-    c1.draw(50,50,100,100)
-    c2.draw(100,50,150,100)
-    c3.draw(100,100,150,150)
-    c1.draw_move(c2)
-    c2.draw_move(c3)
     win.wait_for_close()
 
 main()
--- maze.py
+++ maze.py
@@ -23,6 +23,7 @@ class Maze:
         self._win = win
         self._cells = []
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
         for _ in range(self._num_cols):
@@ -35,10 +36,10 @@ class Maze:
     def _draw_cell(self,i, j):
         if self._win is None:
             return
-        x1 = self._x1 + ( i + 1 ) * self._cell_size_x  
-        y1 = self._y1 + ( j + 1 ) * self._cell_size_y 
+        x1 = self._x1 + i * self._cell_size_x  
+        y1 = self._y1 + j * self._cell_size_y 
         x2 = x1 + self._cell_size_x
-        y2 = y1 + self.cell_size_y 
+        y2 = y1 + self._cell_size_y 
         self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
 
@@ -47,4 +48,10 @@ class Maze:
             return
         self._win.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0,0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1,self._num_rows - 1)
 
